f571be09d14663ec5151889a01c8a9c535fd9e17
chenrui <chenrui@ruoshui.me>
2018-06-22 22:01:43 +0800

    Initial commit

---
+++ src/main/java/com/killtwo/superlibrary/SuperlibraryApplication.java
@@ -0,0 +1,12 @@
+package com.killtwo.superlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperlibraryApplication.class, args);
+	}
+}
